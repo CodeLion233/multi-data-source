f465767f88dd1a32d1115219b9fe1566f5b88b40
CodeLion233 <836213656@qq.com>
2023-05-25 09:43:03 +0800

    feat:多数据源demo2

---
+++ datasource-demo2/src/main/java/com/example/datasourcedemo2/aop/DataSourceAspect.java
@@ -0,0 +1,48 @@
+package com.example.datasourcedemo2.aop;
+
+import com.example.datasourcedemo2.datasource.DynamicDataSource;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class DataSourceAspect implements Ordered {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(DataSourceAspect.class);
+
+    @Pointcut("@annotation(com.example.datasourcedemo2.aop.DataSource)")
+    public void dataSourcePointCut() {}
+
+    @Around("dataSourcePointCut()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) point.getSignature();
+        Method method = methodSignature.getMethod();
+        DataSource ds = method.getAnnotation(DataSource.class);
+        if (ds == null) {
+            DynamicDataSource.setDataSource("db1");
+        } else {
+            DynamicDataSource.setDataSource(ds.value());
+            LOGGER.debug("set datasource is " + ds.value());
+        }
+        try {
+            return point.proceed();
+        } finally {
+            DynamicDataSource.clearDataSource();
+        }
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
